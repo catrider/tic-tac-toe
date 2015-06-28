8faf2a025c807a9e5cc520dd7de479fd154e9a84
Daniel Fischer <d.fischer874@gmail.com>
2015-06-28 13:55:34 -0400

    Added retest plugin to the project

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [robert/hooke "1.1.0"]]
+  :dev-dependencies [[lein-retest "1.0.0"]]
   :main tic-tac-toe.core/start)
